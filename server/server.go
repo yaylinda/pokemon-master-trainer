8376c2b595a7e7224254ec6d2d5951cdba6effea
Linda Zheng <lindazheng1993@gmail.com>
2022-02-14 00:35:16 -0600

    trying to set up go

---
+++ server/server.go
@@ -0,0 +1,20 @@
+import (
+	"log"
+  
+	firebase "firebase.google.com/go"
+	"google.golang.org/api/option"
+  )
+  
+  // Use a service account
+  ctx := context.Background()
+  sa := option.WithCredentialsFile("path/to/serviceAccount.json")
+  app, err := firebase.NewApp(ctx, nil, sa)
+  if err != nil {
+	log.Fatalln(err)
+  }
+  
+  client, err := app.Firestore(ctx)
+  if err != nil {
+	log.Fatalln(err)
+  }
+  defer client.Close()
